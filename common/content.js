f89b709a86a2b54fa89c20508a827e3c6ef67c4d
Tom Anderson <me@neontom.com>
2020-12-07 17:38:20 -0500

    initial commit

---
+++ common/content.js
@@ -0,0 +1,24 @@
+// Inject JS onto page to change player_type from 'site' to 'embed'
+const script = document.createElement("script");
+script.textContent = `
+const origFetch = window.fetch;
+window.fetch = (url, init, ...args) => {
+	try {
+		if (typeof url === "string") {
+			if (url.includes("/access_token")) {
+				url = url.replace("player_type=site", "player_type=embed");
+			} else if (
+				url.includes("/gql") &&
+				init &&
+				typeof init.body === "string" &&
+				init.body.includes("PlaybackAccessToken")
+			) {
+				const newBody = JSON.parse(init.body);
+				newBody.variables.playerType = "embed";
+				init.body = JSON.stringify(newBody);
+			}
+		}
+	} catch (e) {}
+	return origFetch(url, init, ...args);
+};`;
+document.documentElement.prepend(script);
